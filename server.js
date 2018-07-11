c2d8292e54cb51d289f441c328a346b886fab0d4
iamebuka <obukweluebuka@gmail.com>
2018-07-11 16:51:26 +0100

    modified serverjs file to point middleware to client/build folder to serve react file on build

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 const con = mysql.createConnection({
