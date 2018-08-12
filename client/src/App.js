daca09c705bfc39a956389ca513f09811b04a66f
iamebuka <obukweluebuka@gmail.com>
2018-08-12 13:31:01 +0100

    removed state variable rexpense

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,6 @@ class App extends Component {
       note: '',
       category: "",
       expense: [],
-      rexpense: [],
       rawData: [],
       pageLoad: true,
       searchText: ""
@@ -189,7 +188,6 @@ class App extends Component {
       let parseData = this.parseData(sorted)
       this.setState({
         expense: [...parseData],
-        rexpense: [...parseData],
         rawData: [...data],
         pageLoad: false
       })
